4d4565616126d3c2890ca9b261f75b9f29cb3816
christianisita <sitachristiani@gmail.com>
2020-12-01 21:03:58 +0700

    daftar kelas

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,8 +16,21 @@
 
                     {{ __('You are logged in!') }}
                 </div>
+
                 <p class="card-header">Daftar Kelas</h2>
-            </div>
+                <div class="card-body">
+                    <a href=""><b>Big Data dan Analitik</b></a>
+                    <br>
+                    <p>Setiap hari Selasa pukul 13:00</p>
+                    <br><br>
+                    <a href=""><b>Pengalaman Pengguna</b></a>
+                    <br>
+                    <p>Setiap hari Rabu pukul 07:15</p>
+                    <br><br>
+                    <a href=""><b>Pemrograman Berbasis Web</b></a>
+                    <br>
+                    <p>Setiap hari Rabu pukul 10:00</p>
+                </div>
         </div>
     </div>
 </div>
